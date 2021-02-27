1a1f00c07b2d7c239c5ab7fd445678cb4d691454
VladIvanchuk <73890367+VladIvanchuk@users.noreply.github.com>
2021-02-27 02:34:54 +0200

    Add files via upload

--- js/mainjquery.js
+++ js/mainjquery.js
@@ -8,3 +8,28 @@ $(window).scroll(function() {
   $('header').removeClass("sticky");
 }
 });
+
+//Слайдер 
+
+$(document).ready(function(){
+
+	$('.slider').slick({
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		autoplay: true,
+  		autoplaySpeed: 1500,
+  		pauseOnFocus:false,
+  		pauseOnHover:false,
+	});
+});
+
+$(document).ready(function(){
+
+	$('.slider__news').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		dots:true,
+		autoplay: true,
+  		autoplaySpeed: 3000,
+	});
+});
