689da9a9d490dab0f56f3304f9c28a9f7675dbad
VladIvanchuk <73890367+VladIvanchuk@users.noreply.github.com>
2021-02-28 18:42:36 +0200

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,104 @@
+  <div class="mask">
+    <div class="cssload-thecube">
+      <div class="cssload-cube cssload-c1"></div>
+      <div class="cssload-cube cssload-c2"></div>
+      <div class="cssload-cube cssload-c4"></div>
+      <div class="cssload-cube cssload-c3"></div>
+    </div>
+  </div>
+
+  <a id="btnup"></a>
+
+	<header>
+		<div class="container">
+			<div class="header__container">
+
+			<div class="hamburger-menu">
+    <input id="menu__toggle" type="checkbox" />
+    <label class="menu__btn" for="menu__toggle">
+      <span></span>
+    </label>
+
+    <ul id="nav" class="menu__box">
+      <li><a class="menu__item" href="index.php"> Головна</a></li>
+
+      <li><a class="menu__item sub" href="javascript:void(0)" onclick="scroll(0,0)"><i class="fas fa-caret-down"></i> Про нас</a>
+        <ul>
+            <li><a href="#">Історія</a></li>
+            <li><a href="#">Досягнення</a></li>
+            <li><a href="#">Дисципліни</a></li>
+            <li><a href="#">Випускники</a></li>
+            <li><a href="#">Ресурси</a></li>
+        </ul>
+      </li>
+      <li><a class="menu__item" href="entrant.php">Вступнику</a></li>
+      <li><a class="menu__item sub" href="javascript:void(0)" onclick="scroll(0,0)"><i class="fas fa-caret-down"></i> Студенту</a>
+        <ul>
+            <li><a href="#">Співпраця з ВНЗ</a></li>
+            <li><a href="#">Працевлаштування</a></li>
+            <li><a href="#">Викладачі</a></li>
+            <li><a href="#">Оплата навчання</a></li>
+        </ul>
+      </li>
+      <li><a class="menu__item" href="javascript:void(0)" onclick="scroll(0,0)"><i class="fas fa-caret-down"></i> Діяльність</a>
+        <ul>
+            <li><a href="#">Конференції</a></li>
+            <li><a href="#">Олімпіади</a></li>
+        </ul> 
+      </li>
+      
+      <li><a class="menu__item" href="javascript:void(0)" onclick="scroll(0,0)"><i class="fas fa-caret-down"></i> Наші випускники</a>
+        <ul>
+            <li><a href="#">Наукова сфера</a></li>
+            <li><a href="#">Виробнича сфера</a></li>
+            <li><a href="#">Суспільна діяльність</a></li>
+        </ul> 
+      </li>
+      <li><a class="menu__item" href="#">Інформаційні ресурси</a></li>
+    </ul>
+  </div>
+
+
+			<nav class="header__nav nav">
+				<ul class="header__nav__ul">
+					<li class="header__item">
+						<a  href="index.php"><img class="nav__button" src="img/svg/nav (1).svg"><a  id="activ_page" href="index.php" class="header__link">Головна</a></a>
+					</li>
+					<li class="header__item">
+            <div class="dropdown">
+						<a href=""><img class="nav__button" src="img/svg/nav (2).svg"><a href="index.php#about_us" class="header__link">Про нас</a></a>
+            <div class="dropdown-content">
+              <a href="">Історія</a>
+                <a href="">Досягнення</a>
+                <a href="">Дисципліни</a>
+                <a href="">Випускники</a>
+                <a href="">Ресурси</a>
+             </div>
+           </div>
+					</li>
+					<li class="header__item">
+             <div class="dropdown">
+						<a href=""><img class="nav__button" src="img/svg/nav (3).svg"><a href="" class="header__link">Студенту</a></a>
+            <div class="dropdown-content">
+                <a href="">Співпраця з ВНЗ</a>
+                <a href="">Працевлаштування</a>
+                <a href="">Викладачі</a>
+                <a href="">Оплата навчання</a>
+             </div>
+           </div>
+					</li>
+					<li class="header__item">
+						<a href="entrant.php"><img class="nav__button" src="img/svg/nav (4).svg"><a href="entrant.php" class="header__link">Вступнику</a></a>
+					</li>								
+				</ul>
+			</nav>
+
+			<div class="header__menu">
+				<a href="http://nemk.com.ua/" class="nemk__link">
+				<img class="logo__nemk" src="img/NEMK.png" alt="NEMK">
+				</a>
+			</div>
+
+			</div>
+		</div>
+	</header>
